a20559b6be8c48e1c3c4d8cc54a476189ef04257
Rancho <ranchocooper@gmail.com>
2021-03-26 02:10:35 +0800

    array

---
+++ array/sum.go
@@ -0,0 +1,29 @@
+package array
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		// sums[i] = Sum(numbers)
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			numbers = numbers[1:]
+			sums = append(sums, Sum(numbers))
+		}
+	}
+	return
+}
